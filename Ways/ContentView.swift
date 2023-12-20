490d9dfde633f6965f0cf88e974a658a81167488
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2023-12-20 18:59:57 +0800

    Create Xcode project

---
+++ Ways/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Ways
+//
+//  Created by Leon on 2023/12/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ways/WaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaysApp.swift
+//  Ways
+//
+//  Created by Leon on 2023/12/20.
+//
+
+import SwiftUI
+
+@main
+struct WaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
