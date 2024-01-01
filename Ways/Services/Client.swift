e93aa392d93709efb0ac4d8bdef7a52e090b8879
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-01-01 17:05:06 +0800

    Add network interface to temporarily load local json data

---
+++ Ways/Services/Client.swift
@@ -0,0 +1,54 @@
+//
+//  Client.swift
+//  Ways
+//
+//  Created by Leon on 2024/1/1.
+//
+
+import Foundation
+import Dependencies
+
+struct Client {
+    var allWay: () async throws -> [Way]
+    var way: (Int) async throws -> Way
+}
+
+extension Client: DependencyKey {
+    static let liveValue = Self(
+        allWay: loadLocalAllWays,
+        way: loadLocalWay
+    )
+    static let testValue = Self(
+        allWay: loadLocalAllWays,
+        way: loadLocalWay
+    )
+    static let previewValue = Self(
+        allWay: loadLocalAllWays,
+        way: loadLocalWay
+    )
+}
+
+extension DependencyValues {
+    var client: Client {
+        get { self[Client.self] }
+        set { self[Client.self] = newValue }
+    }
+}
+
+fileprivate let loadLocalAllWays: () async throws -> [Way] = {
+    guard let url = Bundle.main.url(forResource: "allWays", withExtension: "json") else {
+        fatalError("Failed to find allWays.json")
+    }
+    let data = try Data(contentsOf: url)
+    let ways = try JSONDecoder().decode([Way].self, from: data)
+    return ways
+}()
+
+fileprivate let loadLocalWay = { _ in
+    guard let url = Bundle.main.url(forResource: "way", withExtension: "json") else {
+        fatalError("Failed to find way.json")
+    }
+    let data = try Data(contentsOf: url)
+    let way = try JSONDecoder().decode(Way.self, from: data)
+    return way
+}(Int)
