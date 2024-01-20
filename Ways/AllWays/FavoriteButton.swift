61d848481353770c540e256666673ce7f18763d5
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-01-20 20:24:16 +0800

    Add "Favorite" (WIP)

---
+++ Ways/AllWays/FavoriteButton.swift
@@ -0,0 +1,18 @@
+//
+//  FavoriteButton.swift
+//  Ways
+//
+//  Created by Leon on 2024/1/20.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FavoriteButton()
+}
---
+++ Ways/AllWays/FavoriteReducer.swift
@@ -0,0 +1,14 @@
+//
+//  FavoriteReducer.swift
+//  Ways
+//
+//  Created by Leon on 2024/1/20.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct FavoriteReducer {
+    
+}
