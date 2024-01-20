0350611391a5f5225c9f2a5d92ae5808b73fe49d
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-01-20 19:58:51 +0800

    Implement a RatingView

--- Ways/Comments/RatingOutlineView.swift
+++ Ways/Comments/RatingOutlineView.swift
@@ -1,19 +1,18 @@
 //
-//  ScoreSumView.swift
+//  RatingOutlineView.swift
 //  Ways
 //
 //  Created by Leon on 2024/1/20.
 //
 
 import SwiftUI
-import ComposableArchitecture
 
-struct ScoreSumView: View {
+struct RatingOutlineView: View {
     var body: some View {
         Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
     }
 }
 
 #Preview {
-    ScoreSumView()
+    RatingOutlineView()
 }
