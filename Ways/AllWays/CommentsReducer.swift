215a8b48637c5b21e4d3de0f7b9e1c0b76aabf96
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-01-20 17:21:14 +0800

    Add "Comments" (WIP)

---
+++ Ways/AllWays/CommentsReducer.swift
@@ -0,0 +1,13 @@
+//
+//  CommentsReducer.swift
+//  Ways
+//
+//  Created by Leon on 2024/1/16.
+//
+
+import Foundation
+import ComposableArchitecture
+
+struct CommentsReducer {
+    
+}
---
+++ Ways/AllWays/CommentsView.swift
@@ -0,0 +1,18 @@
+//
+//  CommentsView.swift
+//  Ways
+//
+//  Created by Leon on 2024/1/16.
+//
+
+import SwiftUI
+
+struct CommentsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CommentsView()
+}
