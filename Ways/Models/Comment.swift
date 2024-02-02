ae897e35fac82ee6cbd20edb519c415620593ca8
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-02-02 21:31:45 +0800

    model Comment needs id

--- Ways/Models/Comment.swift
+++ Ways/Models/Comment.swift
@@ -10,6 +10,7 @@ import SwiftData
 
 @Model
 final class Comment {
+    var id: UUID
     var score: Int
     var subject: String
     var comment: String
@@ -17,6 +18,7 @@ final class Comment {
     var date: Date
     
     init(score: Int, subject: String, date: Date, comment: String, commenter: String) {
+        self.id = UUID()
         self.score = score
         self.subject = subject
         self.date = date
@@ -26,6 +28,7 @@ final class Comment {
     
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        id = try container.decode(UUID.self, forKey: .id)
         score = try container.decode(Int.self, forKey: .score)
         subject = try container.decode(String.self, forKey: .subject)
         comment = try container.decode(String.self, forKey: .comment)
@@ -34,10 +37,9 @@ final class Comment {
     }
 }
 
-
-
 extension Comment: Codable {
     enum CodingKeys: CodingKey {
+        case id
         case score
         case subject
         case comment
@@ -47,6 +49,7 @@ extension Comment: Codable {
     
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(id, forKey: .id)
         try container.encode(score, forKey: .score)
         try container.encode(subject, forKey: .subject)
         try container.encode(comment, forKey: .comment)
