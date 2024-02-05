eaed6930493ee472e5b8643f115981199548ac5e
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-02-05 21:05:02 +0800

    Encapsulate 'NavigateAndLoad' (stub)

---
+++ Ways/Features/NavigateAndLoad/NavigateAndLoadReducer.swift
@@ -0,0 +1,28 @@
+//
+//  NavigateAndLoadReducer.swift
+//  Ways
+//
+//  Created by Leon on 2024/2/5.
+//
+
+import Foundation
+import ComposableArchitecture
+
+@Reducer
+struct NavigateAndLoadReducer {
+    struct State: Equatable {
+        
+    }
+    
+    enum Action {
+        
+    }
+    
+    var body: some ReducerOf<Self> {
+        Reduce { state, action in
+            switch action {
+                
+            }
+        }
+    }
+}
---
+++ Ways/Features/NavigateAndLoad/NavigateAndLoadView.swift
@@ -0,0 +1,18 @@
+//
+//  NavigateAndLoadView.swift
+//  Ways
+//
+//  Created by Leon on 2024/2/5.
+//
+
+import SwiftUI
+
+struct NavigateAndLoadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigateAndLoadView()
+}
