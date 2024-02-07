3bb8d7449c01b94852f6711b7c746dbb5a161628
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-02-07 21:03:29 +0800

    Encapsulate 'Load' (stub)

---
+++ Ways/Features/Load/LoadView.swift
@@ -0,0 +1,18 @@
+//
+//  LoadView.swift
+//  Ways
+//
+//  Created by Leon on 2024/2/7.
+//
+
+import SwiftUI
+
+struct LoadView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoadView()
+}
