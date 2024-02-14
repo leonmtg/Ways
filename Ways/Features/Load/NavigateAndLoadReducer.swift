014a888e3e412c12fdf288d56b2819b53129d6a2
Leon Ma <1886818+leonmtg@users.noreply.github.com>
2024-02-14 20:10:09 +0800

    NavigateAndLoadReducer (WIP)

--- Ways/Features/Load/NavigateAndLoadReducer.swift
+++ Ways/Features/Load/NavigateAndLoadReducer.swift
@@ -8,21 +8,63 @@
 import Foundation
 import ComposableArchitecture
 
+struct NavigateAndLoadState<Content: Reducer>: Equatable where Content.State: Equatable {
+    @PresentationState var alert: AlertState<NavigateAndLoadAction<Content>.Alert>?
+    var isNavigationActive = false
+    var optionalContent: Content.State?
+}
+
+@CasePathable
+enum NavigateAndLoadAction<Content: Reducer> {
+    case optionalContent(Content.Action)
+    case alert(PresentationAction<Alert>)
+    case setNavigation(isActive: Bool)
+    case response(Result<Content.State, Error>)
+    
+    enum Alert: Equatable {}
+}
+
 @Reducer
-struct NavigateAndLoadReducer {
-    struct State: Equatable {
-        
+struct NavigateAndLoadReducer<Content: Reducer> where Content.State: Equatable {
+    let load: @Sendable () async throws -> Content.State
+    
+    private enum CancelID {
+        case load
     }
     
-    enum Action {
-        
-    }
-    
-    var body: some ReducerOf<Self> {
+    var body: some Reducer<NavigateAndLoadState<Content>, NavigateAndLoadAction<Content>> {
         Reduce { state, action in
             switch action {
+            case .alert(.dismiss):
+                state.alert = nil
+                return .none
                 
+            case .setNavigation(isActive: true):
+                state.isNavigationActive = true
+                return .run { send in
+                    await send(.response(Result { try await load() } ))
+                }
+                .cancellable(id: CancelID.load, cancelInFlight: true)
+                
+            case .setNavigation(isActive: false):
+                state.isNavigationActive = false
+                state.optionalContent = nil
+                return .cancel(id: CancelID.load)
+                
+            case let .response(.failure(error)):
+                state.alert = AlertState { TextState(error.localizedDescription) }
+                return .none
+            
+            case let .response(.success(contentState)):
+                state.optionalContent = contentState
+                return .none
+                
+            case .optionalContent:
+                return .none
             }
         }
+//        .ifLet(\.optionalContent, action: \.optionalContent) {
+//            Content()
+//        }
     }
 }
